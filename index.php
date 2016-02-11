2f2ff03c8a196b708edd41a5834a841ee2320601
Chithra-J <chithrajayakumar1@gmail.com>
2016-02-10 21:28:55 -0500

    Adding css file;Updated php page and image.

--- index.php
+++ index.php
@@ -1,15 +1,38 @@
 <html>
-<head>
-	<title>Dynamic Web Applications - Project P1</title>
-</head>
-<body>
+	<head>
+		<title>Dynamic Web Applications - Project P1</title>
+		<link href='css/p1.css' rel='stylesheet'>
+	</head>
+	<body>
 		<h1>Dynamic Web Applications - Project P1</h1>
-		<img src='images/flower.jpg' alt='Chithra Jayakumar'>
-		<h3>Project 2 Title</h3>
-		<p>README.md: [link]</p>
-		<h3>Project 3 Title</h3>
-		<p>README.md: [link]</p>
-		<h3>Project 4 Title</h3>
-		<p>README.md: [link]</p>
-</body>
+		<h2>Chithra Jayakumar</h2>
+		<h3><img src='images/flower.png' alt='Chithra Jayakumar'></h3>
+		<p>
+			Live URL: [link]
+		</p>
+		<p>
+			GitHub: [link]
+		</p>
+		<h3>Project 2</h3>
+		<p>
+			Live URL: [link]
+		</p>
+		<p>
+			GitHub: [link]
+		</p>
+		<h3>Project 3</h3>
+		<p>
+			Live URL: [link]
+		</p>
+		<p>
+			GitHub: [link]
+		</p>
+		<h3>Project 4</h3>
+		<p>
+			Live URL: [link]
+		</p>
+		<p>
+			GitHub: [link]
+		</p>
+	</body>
 </html>
