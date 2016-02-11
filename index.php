ee3ad71a09f81061a2df65e3ea152948fe937850
Chithra-J <chithrajayakumar1@gmail.com>
2016-02-10 19:47:42 -0500

    Adding initial index file and images folder.

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+	<title>Dynamic Web Applications - Project P1</title>
+</head>
+<body>
+		<h1>Dynamic Web Applications - Project P1</h1>
+		<img src='images/flower.jpg' alt='Chithra Jayakumar'>
+		<h3>Project 2 Title</h3>
+		<p>README.md: [link]</p>
+		<h3>Project 3 Title</h3>
+		<p>README.md: [link]</p>
+		<h3>Project 4 Title</h3>
+		<p>README.md: [link]</p>
+</body>
+</html>
